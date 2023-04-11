fbf807bf9579f2a55768431009d2f5bd7df12e48
Tharlo <hadrien2509@gmail.com>
2023-04-11 17:36:03 +0200

    parsing in progress

---
+++ includes/token.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 17:58:46 by hgeissle          #+#    #+#             */
+/*   Updated: 2023/04/11 13:49:35 by hgeissle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKEN_H
+# define TOKEN_H
+
+typedef enum s_TokenType
+{
+	TOKEN_COMMAND,
+	TOKEN_ARGUMENT,
+	TOKEN_REDIRECT_INPUT,
+	TOKEN_REDIRECT_OUTPUT,
+	TOKEN_PIPE,
+	TOKEN_BACKGROUND,
+	TOKEN_END
+}					t_TokenType;
+
+typedef struct s_Token
+{
+	t_TokenType		type;
+	char			*value;
+	struct s_Token	*prev;
+	struct s_Token	*next;
+}					t_Token;
+
+typedef struct s_Parse
+{
+	int				arg_nb;
+	int				stdin;
+	int				stdout;
+	char			**cmd;
+	struct s_Parse	*next;
+}					t_Parse;
+
+#endif
--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 12:45:43 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/04/10 17:43:03 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/04/11 17:26:51 by hgeissle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,39 @@ void	ft_lstaddtoken_back(t_Token **lst, t_Token *new)
 	else
 		*lst = new;
 }
+t_Parse	*ft_lstnewcmd(int arg_nb)
+{
+	t_Parse	*new_lst;
+
+	new_lst = malloc(sizeof(t_Parse));
+	if (!new_lst)
+		return (0);
+	new_lst->arg_nb = 0;
+	new_lst->next = 0;
+	return (new_lst);
+}
+
+t_Parse	*ft_lstlastcmd(t_Parse *lst)
+{
+	while (lst && lst->next)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+void	ft_lstaddcmd_back(t_Parse **lst, t_Parse *new)
+{
+	t_Parse	*last;
+
+	if (*lst)
+	{
+		last = ft_lstlastcmd(*lst);
+		last->next = new;
+	}
+	else
+		*lst = new;
+}
 
 t_Token	create_tokens(char **line)
 {
@@ -85,13 +118,14 @@ t_Token	create_tokens(char **line)
 	int			arg_needed;
 	t_Token		*new;
 	t_Token		*token;
+	t_Parse		*parse;
+	t_Parse		*new_cmd;
 
 	token = 0;
 	i = 0;
 	arg_needed = 0;
 	while (line[i])
 	{
-		ft_lstnewtoken(2, line[i]);
 		if (ft_strcmp(line[i], "<") == 0)
 		{
 			i++;
@@ -105,47 +139,53 @@ t_Token	create_tokens(char **line)
 		else if (ft_strcmp(line[i], "|") == 0)
 		{
 			new = ft_lstnewtoken(4, line[i]);
+			ft_lstaddcmd_back(&parse, new_cmd);
 			arg_needed = 0;
 		}
 		else if (arg_needed == 1)
+		{
 			new = ft_lstnewtoken(1, line[i]);
+			new_cmd->arg_nb++;
+		}
 		else
 		{
 			new = ft_lstnewtoken(0, get_cmd_path(line[i]));
+			new = ft_lstnewcmd(0);
 			arg_needed = 1;
 		}
 		i++;
 		ft_lstaddtoken_back(&token, new);
 	}
+	ft_lstaddcmd_back(&parse, new);
 	return (*token);
 }
 
-// void	parse_tokens(t_Token *token)
-// {
-// 	if (token)
-// }
+void	parse_tokens(t_Token *token, t_Parse *parse)
+{
+	int		i;
 
-// void	create_tokens(char **line, t_instructions *lst)
-// {
-// 	int		i;
-
-// 	i = 0;
-// 	while (line[i])
-// 	{
-// 		if (ft_strcmp(line[i], ">") == 0)
-// 		{
-// 			ft_setcmd();
-// 			ft_setcmdarg();
-// 			ft_setinput();
-// 			ft_setoutput();
-// 		}	
-// 		if (ft_strcmp(line[i], "<") == 0)
-// 		{
-// 			ft_setcmd();
-// 			ft_setcmdarg();
-// 			ft_setinput();
-// 			ft_setoutput();
-// 		}
-// 		if (ft_strcmp(line[i], "") == 0)
-// 	}
-// }
+	i = 0;
+	parse->cmd = malloc(sizeof(char *) * (parse->arg_nb + 2));
+	if (!parse->cmd)
+		return (0);
+	while (token->type != 0)
+	{
+		if (token->type == 0 || token->type == 1)
+			parse->cmd[i++] = token->value;
+		if (token->type == 2)
+		{
+			parse->stdin = open(token->value, O_RDONLY);
+			if (parse->stdin == -1)
+				perror(token->value);
+		}
+		if (token->type == 3)
+		{
+			parse->stdout = open(token->value, O_CREAT | O_WRONLY | O_APPEND, 0644);
+			if (parse->stdout == -1)
+				perror(token->value);
+		}
+		if (token->type == 4)
+			parse->stdout = pipe(end);
+		token = token->next;
+	}
+}
